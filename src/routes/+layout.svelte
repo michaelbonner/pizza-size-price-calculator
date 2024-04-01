821d481334ea0220d8453f1f920f6307269765ac
Michael Bonner <michaelwbonner@gmail.com>
2024-03-31 22:59:50 -0600

    basic pizza calculation functionality

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="py-8 px-8 max-w-7xl mx-auto">
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,98 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	const sizes = ['10', '12', '14', '16', '18', '20'];
+
+	let pizzas = [
+		{ name: 'Dominos Medium', size: '12', price: 6.99 },
+		{ name: 'Little Caesars', size: '14', price: 6.99 },
+		{ name: 'Costco', size: '18', price: 9.95 }
+	];
+
+	const squareInchPerPerson = 40;
+</script>
+
+<h1 class="text-3xl lg:text-5xl font-bold text-white lg:text-center lg:py-8">
+	Pizza Size Price Calculator
+</h1>
+
+<div class="grid lg:grid-cols-3 w-full py-8 gap-8">
+	{#each pizzas as pizza}
+		<div class="bg-yellow-500 p-3 rounded-3xl flex items-center">
+			<div class="w-full grid gap-2 py-8 px-8 bg-white rounded-2xl">
+				<div>
+					<input
+						class="w-full"
+						bind:value={pizza.name}
+						name="name"
+						placeholder="Name"
+						type="text"
+					/>
+				</div>
+				<div class="w-full grid lg:grid-cols-3 gap-6">
+					<div>
+						<select name="size" bind:value={pizza.size}>
+							{#each sizes as size}
+								<option value={size}>{size}&ldquo;</option>
+							{/each}
+						</select>
+					</div>
+					<div class="flex gap-1 items-center lg:col-span-2">
+						<span class="text-gray-600 font-light" style={`scale: 1 1.35`}>$</span>
+						<input
+							bind:value={pizza.price}
+							class="w-full"
+							min="0"
+							name="price"
+							placeholder="Price"
+							step="0.01"
+							type="number"
+						/>
+					</div>
+				</div>
+
+				<hr class="my-4" />
+
+				<div>
+					in<sup>2</sup>: {(Math.PI * Math.pow(+pizza.size / 2, 2)).toFixed(2)}
+				</div>
+				<div>
+					$ / in<sup>2</sup>: ${(+pizza.price / (Math.PI * Math.pow(+pizza.size / 2, 2))).toFixed(
+						2
+					)}
+				</div>
+				<div>
+					Servings / pizza: {(
+						(Math.PI * Math.pow(+pizza.size / 2, 2)) /
+						squareInchPerPerson
+					).toFixed(2)}
+				</div>
+				<div>
+					$ / person: ${(
+						pizza.price /
+						((Math.PI * Math.pow(+pizza.size / 2, 2)) / squareInchPerPerson)
+					).toFixed(2)}
+				</div>
+				<div class="flex justify-end">
+					<button
+						class="py-2 px-4 text-red-700 rounded transition-colors hover:bg-red-600 hover:text-white -mr-4 -mb-4"
+						on:click={() => {
+							pizzas = pizzas.filter((p) => p !== pizza);
+						}}
+					>
+						Delete
+					</button>
+				</div>
+			</div>
+		</div>
+	{/each}
+
+	<div class="bg-yellow-500 p-3 rounded-3xl flex items-stretch">
+		<button
+			class="p-8 w-full h-full min-h-[356px] flex justify-center items-center bg-yellow-50 rounded-2xl transition-colors text-3xl font-bold text-yellow-800 hover:bg-yellow-200"
+			on:click={() => {
+				pizzas = [...pizzas, { name: '', size: '14', price: 0 }];
+			}}
+		>
+			+
+		</button>
+	</div>
+</div>
