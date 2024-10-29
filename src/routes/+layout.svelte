81132045395cf98cafd544f43577eb261cc08229
Michael Bonner <michaelwbonner@gmail.com>
2024-10-29 14:39:59 -0600

    Run rustywind on entire project

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,6 @@
 	import '../app.css';
 </script>
 
-<div class="py-8 px-8 max-w-7xl mx-auto">
+<div class="py-8 px-8 mx-auto max-w-7xl">
 	<slot />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,14 +14,14 @@
 	<title>Pizza Size Price Calculator</title>
 </svelte:head>
 
-<h1 class="text-3xl lg:text-5xl font-bold text-white lg:text-center lg:py-8">
+<h1 class="text-3xl font-bold text-white lg:py-8 lg:text-5xl lg:text-center">
 	Pizza Size Price Calculator
 </h1>
 
-<div class="grid lg:grid-cols-3 w-full py-8 gap-8">
+<div class="grid gap-8 py-8 w-full lg:grid-cols-3">
 	{#each pizzas as pizza}
-		<div class="bg-yellow-500 p-3 rounded-3xl flex items-center">
-			<div class="w-full grid gap-2 py-8 px-8 bg-white rounded-2xl">
+		<div class="flex items-center p-3 bg-yellow-500 rounded-3xl">
+			<div class="grid gap-2 py-8 px-8 w-full bg-white rounded-2xl">
 				<div>
 					<input
 						class="w-full"
@@ -31,7 +31,7 @@
 						type="text"
 					/>
 				</div>
-				<div class="w-full grid lg:grid-cols-2 gap-x-6 gap-y-2">
+				<div class="grid gap-y-2 gap-x-6 w-full lg:grid-cols-2">
 					<div>
 						<select class="w-full" name="size" bind:value={pizza.size}>
 							{#each sizes as size}
@@ -40,7 +40,7 @@
 						</select>
 					</div>
 					<div class="flex gap-1 items-center">
-						<span class="text-gray-600 font-light" style={`scale: 1 1.35`}>$</span>
+						<span class="font-light text-gray-600" style={`scale: 1 1.35`}>$</span>
 						<input
 							bind:value={pizza.price}
 							class="w-full"
@@ -77,7 +77,7 @@
 				</div>
 				<div class="flex justify-end">
 					<button
-						class="py-2 px-4 text-red-700 rounded transition-colors hover:bg-red-600 hover:text-white -mr-4 -mb-4"
+						class="py-2 px-4 -mr-4 -mb-4 text-red-700 rounded transition-colors hover:text-white hover:bg-red-600"
 						on:click={() => {
 							pizzas = pizzas.filter((p) => p !== pizza);
 						}}
@@ -89,9 +89,9 @@
 		</div>
 	{/each}
 
-	<div class="bg-yellow-500 p-3 rounded-3xl flex items-stretch">
+	<div class="flex items-stretch p-3 bg-yellow-500 rounded-3xl">
 		<button
-			class="p-8 w-full h-full min-h-[356px] flex justify-center items-center bg-yellow-50 rounded-2xl transition-colors text-3xl font-bold text-yellow-800 hover:bg-yellow-200"
+			class="flex justify-center items-center p-8 w-full h-full text-3xl font-bold text-yellow-800 bg-yellow-50 rounded-2xl transition-colors hover:bg-yellow-200 min-h-[356px]"
 			on:click={() => {
 				pizzas = [...pizzas, { name: '', size: '14', price: 0 }];
 			}}
