e313dd566ab748405e3b6cacbadc32131f1dc2e2
Michael Bonner <mike@bootpackdigital.com>
2025-05-20 16:38:58 -0600

    Run sv migrate svelte5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
-<script>
+<script lang="ts">
 	import '../app.css';
+
+	let { children } = $props();
 </script>
 
 <div class="py-8 px-8 mx-auto max-w-7xl">
-	<slot />
+	{@render children?.()}
 </div>
